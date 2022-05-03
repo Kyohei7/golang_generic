cfe0d8d53c5dafc2d07e87a4ec0d745079666e1d
Kyohei7 <rizkimuhammad2301@gmail.com>
2022-05-03 23:04:52 +0700

    golang generics 'type inference'

--- type_set_test.go
+++ type_set_test.go
@@ -20,8 +20,8 @@ func Min[T Number](first T, second T) T {
 	}
 }
 func TestMin(t *testing.T) {
-	assert.Equal(t, int(100), Min[int](100, 200))
-	assert.Equal(t, int64(100), Min[int64](100, 200))
-	assert.Equal(t, float64(100.0), Min[float64](100.0, 200.0))
-	assert.Equal(t, Age(100), Min[Age](Age(100), Age(200)))
+	assert.Equal(t, int(100), Min(100, 200))
+	assert.Equal(t, int64(100), Min(int64(100), int64(200)))
+	assert.Equal(t, float64(100.0), Min(100.0, 200.0))
+	assert.Equal(t, Age(100), Min(Age(100), Age(200)))
 }
